6e39005ad6b9d0fc7364bcbf7282560edef04c4c
watermelonpizza <watermelonpizza@melonstudios.net>
2016-01-23 16:08:54 +1100

    start to add movment

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -17,7 +17,23 @@ public class PlayerController : MonoBehaviour
     {
         float vertical = Input.GetAxis("Vertical");
         float horizontal = Input.GetAxis("Horizontal");
-        Vector3 movement = new Vector3(horizontal + vertical, 0, vertical - horizontal);
-        rigidbody.AddForce(movement * 50f);
+        rigidbody.AddForce(CalculateMovement(cameraController.CameraDirection, horizontal, vertical) * 50f);
+    }
+
+    private Vector3 CalculateMovement(CameraDirection cameraDirection, float hoz, float vert)
+    {
+        switch (cameraDirection)
+        {
+            case CameraDirection.North:
+                return new Vector3(hoz + vert, 0, vert - hoz);
+            case CameraDirection.East:
+                return new Vector3(hoz - vert, 0, vert + hoz);
+            case CameraDirection.South:
+                return new Vector3(vert + hoz, 0, hoz + vert);
+            case CameraDirection.West:
+                return new Vector3(vert + hoz, 0, vert - hoz);
+            default:
+                throw new System.Exception("Bad Camera Direction");
+        }
     }
 }
