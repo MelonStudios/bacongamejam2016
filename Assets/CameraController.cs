6aa9b1e3b3cf1c381e237c1491f2ce42244febc7
watermelonpizza <watermelonpizza@melonstudios.net>
2016-01-23 14:02:44 +1100

    Include CameraController

---
+++ Assets/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+
+    }
+}
