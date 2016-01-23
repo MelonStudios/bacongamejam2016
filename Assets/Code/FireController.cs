b7af24f6b9a7b6f2f3051f9d10527089bec91068
watermelonpizza <watermelonpizza@melonstudios.net>
2016-01-23 22:16:18 +1100

    Set FirePoint on player

--- Assets/Code/FireController.cs
+++ Assets/Code/FireController.cs
@@ -3,13 +3,14 @@ using System.Collections;
 
 public class FireController : MonoBehaviour
 {
+    public GameObject FirePoint;
     public GameObject Bullet;
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Instantiate(Bullet, transform.position, transform.rotation);
+            Instantiate(Bullet, FirePoint.transform.position, transform.rotation);
         }
     }
 }
