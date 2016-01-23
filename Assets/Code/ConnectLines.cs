4b8486559283640e9ed739360bae761cfc39f513
watermelonpizza <watermelonpizza@melonstudios.net>
2016-01-24 02:06:03 +1100

    Code cleanup

---
+++ Assets/Code/ConnectLines.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ConnectLines : MonoBehaviour
+{
+    public LineRenderer line;
+    public Transform endPoint;
+
+    // Update is called once per frame
+    void Update()
+    {
+        line.SetPosition(0, transform.position);
+        line.SetPosition(1, endPoint.position);
+    }
+}
