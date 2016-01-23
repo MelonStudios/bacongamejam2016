0bc66eb6f3b8610137aa4760fd581ab63fd49c0a
watermelonpizza <watermelonpizza@melonstudios.net>
2016-01-23 18:11:16 +1100

    Shooting, reorg files

---
+++ Assets/Code/BulletBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BulletBehaviour : MonoBehaviour
+{
+
+
+    public float BulletSpeed;
+    public float RotateSpeed;
+
+    public GameObject BulletMesh;
+
+    void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(transform.forward * BulletSpeed, ForceMode.VelocityChange);
+    }
+
+    void Update()
+    {
+    }
+}
---
+++ Assets/Code/FireController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireController : MonoBehaviour
+{
+    public GameObject Bullet;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(Bullet, transform.position, transform.rotation);
+        }
+    }
+}
