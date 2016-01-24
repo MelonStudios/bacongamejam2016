d0f8f7634a0116a2d2bea79683ab9ea38ea2c25b
Oscar Louw <ojlou1@student.monash.edu>
2016-01-24 21:53:59 +1100

    Basic Main Menu Scene
    Pretty

---
+++ Assets/BobBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BobBehaviour : MonoBehaviour {
+
+    private float bobSpeed;
+    private float bobHeight;
+    private Vector3 startPos;
+    private float counter;
+
+	// Use this for initialization
+	void Start () {
+        bobSpeed = Random.Range(-2f, 2f);
+        bobHeight = Random.Range(0.1f, 3f);
+        startPos = transform.position;
+        counter = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        counter += Time.deltaTime * bobSpeed;
+        transform.position = startPos + Vector3.up * Mathf.Sin(counter) * bobHeight;
+	}
+}
