b6d58916b95cdfa2ddb9a97240ae7d9844a515c8
watermelonpizza <watermelonpizza@melonstudios.net>
2016-01-24 13:02:29 +1100

    Driod controllers

---
+++ Assets/Code/Character.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    protected CharacterState state;
+
+    public CharacterState CharacterState
+    {
+        get { return state; }
+        set
+        {
+            CharacterState oldState = state;
+            state = value;
+
+            if (CharacterStateChanged != null)
+                CharacterStateChanged.Invoke(this, new CharacterStateChangedEventArgs(oldState, state));
+        }
+    }
+
+    public delegate void CharacterStateChangedEventHandler(object sender, CharacterStateChangedEventArgs args);
+    public event CharacterStateChangedEventHandler CharacterStateChanged;
+}
+
+public class CharacterStateChangedEventArgs
+{
+    public CharacterStateChangedEventArgs(CharacterState oldState, CharacterState newState)
+    {
+        OldState = oldState;
+        NewState = newState;
+    }
+
+    public CharacterState OldState { get; set; }
+    public CharacterState NewState { get; set; }
+}
+
+public enum CharacterState
+{
+    Alive,
+    Dead
+}
