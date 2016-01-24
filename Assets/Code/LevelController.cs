68946cb367c85e7cbb1f9ec4f8fce93289515f32
watermelonpizza <watermelonpizza@melonstudios.net>
2016-01-25 03:02:41 +1100

    reset timescale to 1 on level load/restart

--- Assets/Code/LevelController.cs
+++ Assets/Code/LevelController.cs
@@ -5,16 +5,19 @@ public static class LevelController
 {
     public static void RestartLevel()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public static void LoadMainMenu()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene("MainMenu");
     }
 
     public static void LoadLevel(int levelNumber)
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene("Level " + levelNumber);
     }
 }
