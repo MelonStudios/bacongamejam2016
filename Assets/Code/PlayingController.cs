66d4eeb9436a95003d2eb309e9694f1c3e0a338f
Oscar Louw <ojlou1@student.monash.edu>
2016-01-25 19:17:35 +1100

    Added Timer to UI. Changed Icon

--- Assets/Code/PlayingController.cs
+++ Assets/Code/PlayingController.cs
@@ -1,33 +1,48 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayingController : MonoBehaviour
 {
     public Canvas DeathCanvas;
-    
+    public Canvas HUD;
+    public Text timer;
+
     void Awake()
     {
         DeathCanvas.enabled = false;
+        GUI.enabled = true;
+    }
+
+    void Start()
+    {
+        timer.text = GameInformation.Instance.LevelInformation.TimeLimit.ToString();
     }
 
     void Update()
     {
         if (GameInformation.Instance.GameState != GameState.Playing) return;
 
-        GameInformation.Instance.LevelInformation.TimeRemaining -= Time.deltaTime;
+        HUD.enabled = true;
 
+        GameInformation.Instance.LevelInformation.TimeRemaining -= Time.deltaTime;
+        timer.text = Mathf.Round(GameInformation.Instance.LevelInformation.TimeRemaining).ToString();
+        
+        
         if (GameInformation.Instance.PlayerInformation.CharacterState == CharacterState.Dead ||
             GameInformation.Instance.LevelInformation.TimeRemaining <= 0)
         {
             GameInformation.Instance.GameOverResult = GameOverResult.Lose;
             GameInformation.Instance.GameState = GameState.PostGame;
             DeathCanvas.enabled = true;
+            HUD.enabled = false;
         }
         else if (GameObject.FindGameObjectsWithTag("Enemy").Length < 1)
         {
             ScoreController.Instance.CalculateTimeRemainingScore();
             GameInformation.Instance.GameOverResult = GameOverResult.Win;
             GameInformation.Instance.GameState = GameState.PostGame;
+            HUD.enabled = false;
         }
     }
 }
